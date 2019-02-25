5098550552959644901446d184e27b9cc3cbf8df
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-02-25 21:53:02 +0600

    finishing design

--- js/index.js
+++ js/index.js
@@ -7,18 +7,18 @@ player2 = player2 * 6 + 1;
 player2 = Math.floor(player2);
 
 if (player1 > player2) {
-    document.querySelector("#title").innerText = "Player one wins";
+    document.querySelector("#title").innerText = "Player one wins!";
     document.getElementById("pdice1").src = "image/dice/d" + player1+ ".jpg";
     document.getElementById("pdice2").src = "image/dice/d" + player2+ ".jpg";
 
 }
 else if (player2 > player1) {
-    document.querySelector("#title").innerText = "Player two wins";
+    document.querySelector("#title").innerText = "Player two wins!";
     document.getElementById("pdice1").src = "image/dice/d" + player1+ ".jpg";
     document.getElementById("pdice2").src = "image/dice/d" + player2+ ".jpg";
 }
 else {
-    document.querySelector("#title").innerText = "Draw";
+    document.querySelector("#title").innerText = "Draw!";
     document.getElementById("pdice1").src = "image/dice/d" + player1+ ".jpg";
     document.getElementById("pdice2").src = "image/dice/d" + player2+ ".jpg";
 }
